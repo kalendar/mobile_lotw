a3c7e03bb16440df55b16ce9fd3d40e9e7a189fc
Joshua Maddy <110144926+JoshuaMaddy@users.noreply.github.com>
2025-01-06 17:48:48 -0500

    Update env.py to include email_settings field.

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
-from settings import Settings
+from settings import EmailSettings, Settings
 
 ## Populate these settings values!
 
@@ -8,4 +8,9 @@ SETTINGS = Settings(
     QSO_url="https://lotw.arrl.org/lotwuser/qsos?qso_query=1&awg_id=DXCC&ac_acct=1&qso_callsign=&qso_owncall=&qso_startdate=&qso_starttime=&qso_enddate=&qso_endtime=&qso_mode=&qso_band=&qso_qsl=yes&qso_dxcc=&qso_sort=QSL+Date&qso_descend=yes&acct_sel=DXCC%3B1",
     database_key="",
     session_key="",
+    email_settings=EmailSettings(
+        sender_address="",
+        SMTP_address="",
+        SMTP_port=25,
+    ),
 )
